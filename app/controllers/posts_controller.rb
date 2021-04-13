af0e8e902df9787a3fcf620d37de5d1643eda965
oky-128 <matsuokayuta.128.oky@gmail.com>
2021-04-13 11:24:21 +0000

    取引一覧の編集・削除を追加

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,45 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    # 重複した名前を一つにまとめてる
+    @posts = Post.select(:name).distinct
   end
   
   def show
     @posts = Post.where(name: params[:name])
+    
+    # moneyだけの配列を作る
+    @money_array = @posts.pluck(:money)
+  end
+  
+  def new
+  end
+  
+  def create
+    @post = Post.new(date: params[:date], name: params[:name], money: params[:money],
+                     job: params[:job], purpose: params[:purpose], memo: params[:memo])
+    @post.save
+    redirect_to("/index")
+  end
+  
+  def edit
+    @post = Post.find_by(id: params[:id])
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    @post.date = params[:date]
+    @post.name = params[:name]
+    @post.money = params[:money]
+    @post.job = params[:job]
+    @post.purpose = params[:purpose]
+    @post.memo = params[:memo]
+    @post.save
+    redirect_to "/index"
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to "/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,9 @@ Rails.application.routes.draw do
   
   get "index" => "posts#index"
   get "show" => "posts#show"
+  get "new" => "posts#new"
+  post "create" => "posts#create"
+  get "posts/:id/edit" => "posts#edit"
+  post "posts/:id/update" => "posts#update"
+  delete "posts/:id" => "posts#destroy"
 end
